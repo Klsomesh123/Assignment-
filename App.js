76bd158edce002a73d2344831e3cdcb841a2c240
Somesh Krishnan <86156613+Klsomesh123@users.noreply.github.com>
2022-03-26 11:55:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,42 +1,15 @@
-import React, { Component } from "react";
-import "./App.css";
-
+import { Component } from "react";
+import Votingtable from "./Votingtable";
 class App extends Component {
   render() {
     return (
-      <div className="main">
-        <h1>
-          <center>Register New Account</center>
-        </h1>
-        <div className="row-1">
-          <div>
-            <input type="text" placeholder="First Name"></input>
-          </div>
-
-          <div>
-            <input type="text" placeholder="Last Name"></input>
-          </div>
-        </div>
-        <div className="row-2">
-          <input type="text" placeholder="Name"></input>
-        </div>
-        <div className="row-3">
-          <input type="text" placeholder="City"></input>
-        </div>
-        <div className="row-4">
-          <input type="text" placeholder="Phone Number"></input>
-        </div>
-        <div className="row-5">
-          <input type="email" placeholder="E-mail"></input>
-        </div>
-        <div className="create-account-button">
-          <div>
-            <button className="create-account" onClick={this.handleCreate}>
-              Create Account
-            </button>
-          </div>
-        </div>
-      </div>
+      <span>
+        <h2 class="heading">Voting</h2>
+        <Votingtable language="C" />
+        <Votingtable language="C++" />
+        <Votingtable language="Java" />
+        <Votingtable language="Python" />
+      </span>
     );
   }
 }
