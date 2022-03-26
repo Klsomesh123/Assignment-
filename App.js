8ec514745ad7b542639b4f20280ec244ace41cbf
Somesh Krishnan <86156613+Klsomesh123@users.noreply.github.com>
2022-03-26 12:34:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,189 +1,140 @@
 import React, { Component } from "react";
 import "./App.css";
+
 class App extends Component {
   state = {
-    title: "",
-    fname: "",
-    lname: "",
-    date: "",
-    email: "",
-    password: "",
-    confirmpassword: "",
+    name: "",
+    age: "",
+    gender: "",
+    source: "",
+    destination: "",
     submit: false,
+    checkdecision: false,
     checked: false,
-    agreed: false,
-    warning: "",
+    checkboxvalue: "",
   };
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  handleRegister = () => {
-    if (this.state.password === "" && this.state.confirmpassword === "") {
-      alert("Passwords should not be empty");
-      this.setState({ submit: false });
-    } else if (this.state.password !== this.state.confirmpassword) {
-      alert("Password and Confirm Password is mismatch");
-    } else if (this.state.agreed === false) {
-      this.setState({ warning: "*Please accept the terms and Conditions" });
+  handleCheckClick = () => {
+    this.setState({ checkdecision: !this.state.checkdecision });
+  };
+  handleSubmit = () => {
+    if (this.state.source === "" && this.state.destination === "") {
+      alert("Source and Destination should not be empty");
+    } else if (this.state.source === this.state.destination) {
+      alert("Source and Destination should not be same");
+    } else if (this.state.checked === false) {
+      this.setState({ checkboxvalue: "Disagree" });
+      this.setState({ submit: true });
     } else {
+      this.setState({ checkboxvalue: "Agree" });
       this.setState({ submit: true });
     }
   };
-  handleCheckClick = () => {
-    this.setState({ agreed: !this.state.agreed });
-  };
-
-  handleReset = () => {
-    this.setState({
-      title: "",
-      fname: "",
-      lname: "",
-      date: "",
-      email: "",
-      password: "",
-      confirmpassword: "",
-    });
-    this.setState({ submit: false });
-  };
-
   render() {
     return (
       <div>
         <div>
-          <div>
-            {!this.state.submit && (
-              <div class="form">
-                <div class="row-1">
-                  <div class="div-title">
-                    <label class="label-title">Title</label>
-                    <div>
-                      <select
-                        class="title-dropdown"
-                        name="title"
-                        value={this.state.title}
-                        onChange={this.handleChange}
-                      >
-                        <option>Mr.</option>
-                        <option>Ms.</option>
-                        <option>Mrs.</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="div-firstname">
-                    <label class="label-firstname">First Name </label>
-                    <div>
-                      <input
-                        type="text"
-                        class="input-firstname"
-                        name="fname"
-                        value={this.state.fname}
-                        onChange={this.handleChange}
-                      ></input>
-                    </div>
-                  </div>
-                  <div class="div-lastname">
-                    <div>
-                      <label class="label-lastname">Last Name</label>
-                    </div>
-                    <input
-                      type="text"
-                      class="input-lastname"
-                      name="lname"
-                      value={this.state.lname}
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div class="row-2">
-                  <div class="div-dateofbirth">
-                    <div>
-                      <label class="label-dateofbirth">Date of Birth</label>
-                    </div>
-                    <input
-                      type="date"
-                      class="input-dateofbirth"
-                      name="date"
-                      value={this.state.date}
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                  <div class="div-email">
-                    <div>
-                      <label class="label-email">Email</label>
-                    </div>
-                    <input
-                      type="email"
-                      class="input-email"
-                      name="email"
-                      value={this.state.email}
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div class="row-3">
-                  <div class="div-password">
-                    <div>
-                      <label class="label-password">
-                        Password
-                      </label>
-                    </div>
-                    <input
-                      type="password"
-                      class="input-password"
-                      name="password"
-                      value={this.state.password}
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                  <div class="div-confirmpassword">
-                    <div>
-                      <label class="label-confirmpassword">
-                        Confirm Password
-                      </label>
-                    </div>
-                    <input
-                      type="password"
-                      class="input-confirmpassword"
-                      name="confirmpassword"
-                      value={this.state.confirmpassword}
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div class="div-checkbox">
-                  <input
-                    type="checkbox"
-                    checked={this.state.agreed}
-                    onChange={this.handleCheckClick}
-                  ></input>
-                  <label>
-                    Accept Terms & Conditions
-                  </label>
-                  <p class="warning-message">
-                    {!this.state.agreed && this.state.warning}
-                  </p>
-                </div>
-                <div class="div-buttons">
-                  <button onClick={this.handleRegister} class="register-button">
-                    Register
-                  </button>
-                  <button onClick={this.handleReset} class="reset-button">
-                    Reset
-                  </button>
-                </div>
+          {!this.state.submit && (
+            <div class="form">
+              <div class="div-name">
+                <label>Name of the Passenger</label>
+                <input
+                  type="text"
+                  class="input-name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                ></input>
               </div>
-            )}
-          </div>
-
+              <div class="div-age">
+                <label>Age</label>
+                <input
+                  type="number"
+                  class="input-age"
+                  name="age"
+                  value={this.state.age}
+                  onChange={this.handleChange}
+                ></input>
+              </div>
+              <div class="div-gender">
+                <label>Gender</label>
+                <span class="span-gender">
+                  <input
+                    type="radio"
+                    name="gender"
+                    onChange={this.handleChange}
+                    value="Male"
+                  ></input>
+                  <label>Male</label>
+                  <input
+                    type="radio"
+                    class="radio-female"
+                    name="gender"
+                    onChange={this.handleChange}
+                    value="Female"
+                  ></input>
+                  <label>Female</label>
+                </span>
+              </div>
+              <div class="div-source">
+                <label>Source</label>
+                <select
+                  class="dropdown-source"
+                  name="source"
+                  onChange={this.handleChange}
+                  value={this.state.source}
+                >
+                  <option></option>
+                  <option>Chennai</option>
+                  <option>Bangalore</option>
+                  <option>Hyderabad</option>
+                </select>
+              </div>
+              <div class="div-destination">
+                <label>Destination</label>
+                <select
+                  class="dropdown-destination"
+                  name="destination"
+                  onChange={this.handleChange}
+                  value={this.state.destination}
+                >
+                  <option></option>
+                  <option>Chennai</option>
+                  <option>Bangalore</option>
+                  <option>Hyderabad</option>
+                </select>
+              </div>
+              <div class="div-termsandconditions">
+                <input
+                  type="checkbox"
+                  checked={this.state.checkdecision}
+                  onChange={this.handleCheckClick}
+                ></input>
+                <label class="label-termsandconditions">
+                  I Agree to all the Terms and Conditions
+                </label>
+              </div>
+              <div class="div-submit">
+                <button onClick={this.handleSubmit} class="button-submit">
+                  Submit
+                </button>
+              </div>
+            </div>
+          )}
           {this.state.submit && (
             <div>
-              <h2>Registeration Form</h2>
-              Name : {this.state.title + this.state.fname + this.state.lname}
-              <br />
-              Date of Birth : {this.state.date}
-              <br />
-              Email : {this.state.email}
-              <br />I Agreed to the Terms & Conditions
+              <h2>Ticket Booking</h2>
+              <div>Name: {this.state.name}</div>
+              <div>Age: {this.state.age}</div>
+              <div>Gender:{this.state.gender}</div>
+              <div>Source: {this.state.source}</div>
+              <div>Destination:{this.state.destination}</div>
+              <div>
+                I {this.state.checkboxvalue} to the terms and conditions
+              </div>
+              <h2>{this.state.checked}</h2>
             </div>
           )}
         </div>
